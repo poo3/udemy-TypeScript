3dd3e84be985366bb3854cfb65496bb962e20201
バントウフウマ <coolishpoo3@gmail.com>
2021-03-16 12:07:17 +0900

    add class Teacher

--- dist/class.js
+++ dist/class.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(name, age) {
         this.name = name;
@@ -12,5 +27,19 @@ var Person = /** @class */ (function () {
     };
     return Person;
 }());
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(name, age, subject) {
+        var _this = _super.call(this, name, age) || this;
+        _this.subject = subject;
+        return _this;
+    }
+    Teacher.prototype.greeting = function () {
+        console.log("Hello! My name is " + this.name + ". I am " + this.age + " years old. I teach " + this.subject + ".");
+    };
+    return Teacher;
+}(Person));
 var Bob = new Person("Bob", 24);
 console.log(Bob);
+var Washizawa = new Teacher("Washizawa", 30, "science");
+Washizawa.greeting();
--- src/class.ts
+++ src/class.ts
@@ -1,14 +1,29 @@
 class Person {
-  constructor(public name: string,private age: number) {}
+  constructor(public name: string, protected age: number) {}
 
-  incrementAge(){
+  incrementAge() {
     this.age += 1;
   }
 
-  greeting(this: Person){
+  greeting(this: Person) {
     console.log(`Hello! My name is ${this.name}. I am ${this.age} years old `);
   }
 }
 
-const Bob = new Person("Bob",24);
+class Teacher extends Person {
+  constructor(name: string, age: number, public subject: string) {
+    super(name, age);
+  }
+
+  greeting(this: Teacher) {
+    console.log(
+      `Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}.`
+    );
+  }
+}
+
+const Bob = new Person("Bob", 24);
 console.log(Bob);
+
+const Washizawa = new Teacher("Washizawa", 30, "science");
+Washizawa.greeting();
