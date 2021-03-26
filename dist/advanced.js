056d336e9271560f755f115a4e3c6d0d3079c19c
バントウフウマ <coolishpoo3@gmail.com>
2021-03-26 16:38:50 +0900

    add TypeGuardTest

--- dist/advanced.js
+++ dist/advanced.js
@@ -7,3 +7,48 @@ var Jorge = {
 var MixType = 8080;
 console.log(MixType);
 console.log(Jorge.name);
+function toUpperCase(x) {
+    if (typeof x === "string") {
+        return x.toUpperCase;
+    }
+    return;
+}
+function descibeProfile(nomadWorker) {
+    if ("role" in nomadWorker) {
+        console.log(nomadWorker.role);
+    }
+    if ("follower" in nomadWorker) {
+        console.log(nomadWorker.follower);
+    }
+}
+var Jack = {
+    name: "suparou",
+    role: "programer",
+};
+descibeProfile(Jack);
+var Dog = /** @class */ (function () {
+    function Dog() {
+    }
+    Dog.prototype.speak = function () {
+        console.log("bow-wow!");
+    };
+    return Dog;
+}());
+var Bird = /** @class */ (function () {
+    function Bird() {
+    }
+    Bird.prototype.speak = function () {
+        console.log("tweet-tweet");
+    };
+    Bird.prototype.fly = function () {
+        console.log("flutter");
+    };
+    return Bird;
+}());
+function havePet(pet) {
+    pet.speak();
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+havePet(new Bird());
--- src/advanced.ts
+++ src/advanced.ts
@@ -25,3 +25,51 @@ const MixType = 8080;
 
 console.log(MixType);
 console.log(Jorge.name);
+
+function toUpperCase(x: string | number) {
+  if (typeof x === "string") {
+    return x.toUpperCase;
+  }
+  return;
+}
+
+type NomadWorker = Enginner | Blogger;
+function descibeProfile(nomadWorker: NomadWorker) {
+  if ("role" in nomadWorker) {
+    console.log(nomadWorker.role);
+  }
+  if ("follower" in nomadWorker) {
+    console.log(nomadWorker.follower);
+  }
+}
+
+const Jack: Enginner = {
+  name: "suparou",
+  role: "programer",
+};
+
+descibeProfile(Jack);
+
+class Dog {
+  speak() {
+    console.log("bow-wow!");
+  }
+}
+
+class Bird {
+  speak() {
+    console.log("tweet-tweet");
+  }
+  fly() {
+    console.log("flutter");
+  }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+  pet.speak();
+  if (pet instanceof Bird) {
+    pet.fly();
+  }
+}
+havePet(new Bird());
