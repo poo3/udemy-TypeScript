f1dda7db838962e9bde36c5b8e3f883bfd97c037
バントウフウマ <coolishpoo3@gmail.com>
2021-03-31 17:24:25 +0900

    add generics class

--- dist/generics.js
+++ dist/generics.js
@@ -3,3 +3,25 @@ function copy(value, key) {
     return value;
 }
 console.log(copy({ name: "Jorge", age: 33 }, "age"));
+var LightDataBase = /** @class */ (function () {
+    function LightDataBase() {
+        this.data = [];
+    }
+    LightDataBase.prototype.add = function (item) {
+        this.data.push(item);
+    };
+    LightDataBase.prototype.remove = function (item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    LightDataBase.prototype.get = function () {
+        return this.data;
+    };
+    return LightDataBase;
+}());
+var stringLightDataBase = new LightDataBase();
+stringLightDataBase.add("string");
+console.log(stringLightDataBase.get());
+stringLightDataBase.remove("string");
+stringLightDataBase.add("newItem");
+stringLightDataBase.add("secondItem");
+console.log(stringLightDataBase.get());
--- src/generics.ts
+++ src/generics.ts
@@ -6,3 +6,24 @@ function copy<T extends { name: string }, U extends keyof T>(
 }
 
 console.log(copy({ name: "Jorge", age: 33 }, "age"));
+
+class LightDataBase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get() {
+    return this.data;
+  }
+}
+
+const stringLightDataBase = new LightDataBase<string>();
+stringLightDataBase.add("string");
+console.log(stringLightDataBase.get());
+stringLightDataBase.remove("string");
+stringLightDataBase.add("newItem");
+stringLightDataBase.add("secondItem");
+console.log(stringLightDataBase.get());
