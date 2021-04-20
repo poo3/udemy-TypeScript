112970fb5ec99047a060edcff9093b2cef9065cb
バントウフウマ <coolishpoo3@gmail.com>
2021-04-20 17:42:57 +0900

    define Foods class

---
+++ src/food-app.ts
@@ -0,0 +1,9 @@
+class Score {}
+class Food {}
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>("food");
+  constructor() {
+    this.elements.forEach((element) => {});
+  }
+}
+const foods = new Foods();
