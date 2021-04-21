45fe3bb489798adb6c8d0ca7435790db7bd2211a
バントウフウマ <coolishpoo3@gmail.com>
2021-04-21 16:49:38 +0900

    add func get activeElements

--- dist/food-app.js
+++ dist/food-app.js
@@ -17,10 +17,25 @@ var Food = /** @class */ (function () {
 var Foods = /** @class */ (function () {
     function Foods() {
         this.elements = document.querySelectorAll(".food");
+        this._activeElements = [];
         this.elements.forEach(function (element) {
             new Food(element);
         });
     }
+    Object.defineProperty(Foods.prototype, "activeElements", {
+        get: function () {
+            var _this = this;
+            this._activeElements = [];
+            this.elements.forEach(function (element) {
+                if (element.classList.contains("food--active")) {
+                    _this._activeElements.push(element);
+                }
+            });
+            return this._activeElements;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Foods;
 }());
 var foods = new Foods();
